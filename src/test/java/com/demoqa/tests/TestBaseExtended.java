e02e44c747b1ff4be500760f7946a867cde0a9de
Kamila Adigamova <uuueesi@gmail.com>
2023-04-11 17:32:02 +0200

    added all parameters

--- src/test/java/com/demoqa/tests/TestBaseExtended.java
+++ src/test/java/com/demoqa/tests/TestBaseExtended.java
@@ -7,6 +7,7 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
@@ -14,15 +15,20 @@ import java.util.Map;
 public class TestBaseExtended {
 
     @BeforeAll
+    @Tag("main")
     static void configure() {
+        // parametrized values:
         String websiteName = System.getProperty("website", "google.com");
+        String remoteBrowserName = System.getProperty("remote_browser", "abc.com");
+        String browserName = System.getProperty("browser_name", "chrome");
+        String browserVersion = System.getProperty("browser_version", "100.0");
+        String screenResolution = System.getProperty("screen_resolution", "1920x1080");
 
         Configuration.baseUrl = websiteName;
-        Configuration.browserSize = "1920x1080";
-        Configuration.browserPosition = "0x0";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-        Configuration.browser = "chrome";
-        Configuration.browserVersion = "100.0";
+        Configuration.browserSize = screenResolution;
+        Configuration.remote = "https://user1:1234@" + remoteBrowserName + "/wd/hub";
+        Configuration.browser = browserName;
+        Configuration.browserVersion = browserVersion;
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
